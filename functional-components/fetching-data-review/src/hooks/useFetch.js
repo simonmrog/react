f9c99bebcc37275203b9cc273a2497f217d99b6c
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2022-03-17 22:06:17 -0500

    using reducer infetch custom hook

--- functional-components/fetching-data-review/src/hooks/useFetch.js
+++ functional-components/fetching-data-review/src/hooks/useFetch.js
@@ -1,28 +1,44 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return { ...state, isLoading: true, error: null };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        data: action.payload,
+      };
+    case "FETCH_FAILURE":
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      throw new Error("Invalid action for data fetch reducer");
+  }
+};
 
 export const useFetch = (initUrl, method = "get") => {
-  const [data, setData] = useState(null);
   const [url, setUrl] = useState(initUrl);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    data: null,
+    isLoading: false,
+    error: null,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
       try {
-        setIsLoading(true);
         const result = await axios[method](url);
-        setData(result.data);
-        setError(null);
-      } catch (e) {
-        setError(e);
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAILURE", payload: err });
       }
     };
-
     fetchData();
   }, [url, method]);
 
-  return [{ data, isLoading, error }, setUrl];
+  return [state, setUrl];
 };
